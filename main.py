60e34301ca925ad5c1fe48b3d769deeeca1ddefe
Fatih Altun <fatih.altun@pardus.org.tr>
2020-02-02 23:40:52 +0300

    Add menubar,argv actions and Improve code
    Signed-off-by: Fatih Altun <fatih.altun@pardus.org.tr>

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ VERSION_OUTDATED = 1
 VERSION_SAME = 2
 
 """
-import gi,apt,os
+import gi,apt,os,sys
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from gi.repository import GObject
@@ -73,18 +73,28 @@ def main():
             return True
         
     
-    debianpackage = "/home/fatih/Desktop/pdebi/game16.deb"
+    # debianpackage = "/home/pardus/Masaüstü/pdebi/black.deb"
+        
+    debianpackage = ""
     
-    package = aptdeb.DebPackage(debianpackage)
+    installable = False
     
-    packagename = package.pkgname
+    packagename = ""
+        
+    def start(debpackage):
     
-    firststatus = package.compare_to_version_in_cache()
-    
-    installable = package.check()
-    
-    packageversion = package._sections["Version"]
-    packagedescription = package._sections["Description"]
+        package = aptdeb.DebPackage(debpackage)
+        
+        packagename = package.pkgname
+        
+        firststatus = package.compare_to_version_in_cache()
+        
+        installable = package.check()
+        
+        packageversion = package._sections["Version"]
+        packagedescription = package._sections["Description"]
+        
+        return package,packagename,firststatus,installable,packageversion,packagedescription
     
     
     def cacheControl():
@@ -97,6 +107,7 @@ def main():
         if installable:
     
             spinner.start()
+            progress.set_text("Installing..")
             
             button1.set_sensitive(False)
             button2.set_sensitive(False)
@@ -113,6 +124,7 @@ def main():
         if cache[packagename].is_installed:
             
             spinner.start()
+            progress.set_text("Uninstalling..")
             
             button1.set_sensitive(False)
             button2.set_sensitive(False)
@@ -126,6 +138,7 @@ def main():
     def reinstallPackage():
     
         spinner.start()
+        progress.set_text("Reinstalling..")
         
         button1.set_sensitive(False)
         button2.set_sensitive(False)
@@ -139,6 +152,7 @@ def main():
     def downgradePackage():
         
         spinner.start()
+        progress.set_text("Downgrading..")
         
         button1.set_sensitive(False)
         button2.set_sensitive(False)
@@ -155,6 +169,8 @@ def main():
             Gtk.main_quit()
     
         def onButton1Clicked(self, button1):
+            
+            print("debianpackage = " + debianpackage)
             
             packagestatus = cacheControl()
             
@@ -177,20 +193,43 @@ def main():
         def onButton2Clicked(self, button2):
                 print("Uninstalling Button Clicked")
                 removePackage()
+
+        def onOpenClicked(self, openbutton):
+            filechooser.run()
+            filechooser.hide()
+            print("Open Button Clicked")
+            
+        def onSelectClicked(self, openbutton):
+            filename = filechooser.get_filename()
+            filechooser.hide()
+            fromFile(filename)
+            print("Select Button Clicked")    
+            
+        def onActivated(self, openbutton):
+            filename = filechooser.get_filename()
+            filechooser.hide()
+            fromFile(filename)
+            print("Active Button Clicked")  
+        
     
     builder = Gtk.Builder()
-    builder.add_from_file("/home/fatih/Desktop/pdebi/main.glade")
+    builder.add_from_file("/home/pardus/Masaüstü/pdebi/main.glade")
     builder.connect_signals(Handler())
     
     window = builder.get_object("mainwindow")
     
     button1 = builder.get_object("button1")
     button2 = builder.get_object("button2")
-    
+    openbutton = builder.get_object("openbutton")
+    filechooser = builder.get_object("filechooser")
+    selectbutton = builder.get_object("selectbutton")
+
+
     label1 = builder.get_object("label1")
     label2 = builder.get_object("label2")
     
     spinner = builder.get_object("spinner")
+    progress = builder.get_object("progress")
     
     textview = builder.get_object("textview")
     
@@ -198,6 +237,10 @@ def main():
     upgradeicon = builder.get_object("upgrade_icon")
     downgradeicon = builder.get_object("downgrade_icon")
     reinstallicon = builder.get_object("reinstall_icon")
+    
+    installed_version = builder.get_object("installed_version")
+    installed_version_title = builder.get_object("installed_version_title")
+    
     
     
     def packageMain(actioned,status):
@@ -226,19 +269,19 @@ def main():
         
         else:
             
-            if firststatus == 0:
+            if status == 0:
                 button1.set_sensitive(True)
                 button1.set_label("Install")
                 button2.set_sensitive(False)
-            elif firststatus == 1:
+            elif status == 1:
                 button1.set_sensitive(True)
                 button1.set_label("Downgrade")
                 button1.set_image(downgradeicon)
-            elif firststatus == 2:
+            elif status == 2:
                 button1.set_sensitive(True)
                 button1.set_label("Reinstall")
                 button1.set_image(reinstallicon)
-            elif firststatus == 3:
+            elif status == 3:
                 button1.set_sensitive(True)
                 button1.set_label("Upgrade")
                 button1.set_image(upgradeicon)
@@ -251,7 +294,10 @@ def main():
         if retval == 0:
             textview.get_buffer().insert(textview.get_buffer().get_end_iter(),  "Operation was successfully completed ! \n \n")
             textview.scroll_to_iter(textview.get_buffer().get_end_iter(), 0.0, False, 0.0,0.0)
-        packageMain(True,cacheControl())
+        status = cacheControl()
+        packageMain(True,status)
+        getInstalledVersion(status)
+        progress.set_text("Completed")
     
     def on_stdout_data(sender, line):
         textview.get_buffer().insert(textview.get_buffer().get_end_iter(),  line)
@@ -267,10 +313,91 @@ def main():
     spawn.connect("stdout-data", on_stdout_data)
     spawn.connect("stderr-data", on_stderr_data)
     
-    label1.set_text(packagename + " ( " + packageversion + " )")
-    label2.set_text("Description : " + packagedescription )
     
-    packageMain(False,5)
+    def getInstalledVersion(status):
+        
+        if status != 0:
+
+            cache = apt.Cache()
+            pkg = cache[packagename]
+            systemversion = pkg.versions[0].version
+            installed_version.set_text(systemversion)
+            
+        else:
+            installed_version.set_text("Not installed")
+
+    
+
+    if len(sys.argv) > 1:
+        
+        debianpackage = sys.argv[1]
+        aa = start(debianpackage)
+        
+        firststatus = aa[2]
+        packagename = aa[1]
+        installable = aa[3]
+        packageversion =  aa[4]
+        packagedescription = aa[5]
+    
+
+        label1.set_text(packagename + " ( " + packageversion + " )")
+        label2.set_text(packagedescription )
+        
+        if firststatus != 0:
+            cache = apt.Cache()
+            pkg = cache[packagename]
+            systemversion = pkg.versions[0].version
+            installed_version.set_text(systemversion)
+            
+        packageMain(False,firststatus)
+        
+    else:
+        
+        button1.set_sensitive(False)
+        button2.set_sensitive(False)
+        button1.set_label("Install")
+        button2.set_label("Uninstall")
+        label1.set_text("")
+        installed_version_title.set_text("")
+        installed_version.set_text("")
+        
+        
+    def fromFile(path):
+        
+        nonlocal debianpackage,packagename,installable,packageversion,packagedescription,firststatus
+        
+        debianpackage = path
+
+        aa = start(debianpackage)
+        
+        firststatus = aa[2]
+        packagename = aa[1]
+        installable = aa[3]
+        packageversion =  aa[4]
+        packagedescription = aa[5]
+    
+
+        label1.set_text(packagename + " ( " + packageversion + " )")
+        label2.set_text(packagedescription )
+        progress.set_text("")
+        
+        if firststatus != 0:
+            cache = apt.Cache()
+            pkg = cache[packagename]
+            systemversion = pkg.versions[0].version
+            installed_version_title.set_text("Installed Version : ")
+            installed_version.set_text(systemversion)
+        else:
+            installed_version_title.set_text("Installed Version : ")
+            installed_version.set_text("Not installed")
+            
+        packageMain(False,firststatus)
+        
+        
+    
+
+
+    
     
     window.show_all()
     
