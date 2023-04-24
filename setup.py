bd942e409b4c8edc69567192629422ef13f6967a
Fatih Altun <fatih.altun@pardus.org.tr>
2023-04-24 11:54:34 +0300

    Update translation structure

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages, os
+import os
+import subprocess
+from setuptools import setup, find_packages
+
+
+def create_mo_files():
+    podir = "po"
+    mo = []
+    for po in os.listdir(podir):
+        if po.endswith(".po"):
+            os.makedirs("{}/{}/LC_MESSAGES".format(podir, po.split(".po")[0]), exist_ok=True)
+            mo_file = "{}/{}/LC_MESSAGES/{}".format(podir, po.split(".po")[0], "pardus-package-installer.mo")
+            msgfmt_cmd = 'msgfmt {} -o {}'.format(podir + "/" + po, mo_file)
+            subprocess.call(msgfmt_cmd, shell=True)
+            mo.append(("/usr/share/locale/" + po.split(".po")[0] + "/LC_MESSAGES",
+                       ["po/" + po.split(".po")[0] + "/LC_MESSAGES/pardus-package-installer.mo"]))
+    return mo
+
 
 changelog = "debian/changelog"
+version = "0.1.0"
 if os.path.exists(changelog):
     head = open(changelog).readline()
     try:
         version = head.split("(")[1].split(")")[0]
     except:
         print("debian/changelog format is wrong for get version")
-        version = ""
     f = open("src/__version__", "w")
     f.write(version)
     f.close()
@@ -24,7 +41,7 @@ data_files = [
     ("/usr/bin", ["pardus-package-installer"]),
     ("/usr/share/polkit-1/actions", ["tr.org.pardus.pkexec.pardus-package-installer.policy"]),
     ("/usr/share/icons/hicolor/scalable/apps/", ["images/pardus-package-installer.svg"])
-]
+] + create_mo_files()
 
 setup(
     name="Pardus Package Installer",
@@ -38,5 +55,5 @@ setup(
     description="Pardus Deb Package Installer.",
     license="GPLv3",
     keywords="deb package installer",
-    url="https://www.pardus.org.tr",
+    url="https://github.com/pardus/pardus-package-installer",
 )
